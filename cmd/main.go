a77af1203a2e3c8a2536eb6c77b1402c59696726
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-30 19:27:50 +0330

    refactor: enhance database connection handling and add user migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/VeluraDoc/Velura-Backend-Main/config"
+	"github.com/VeluraDoc/Velura-Backend-Main/internal/model"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv()
-	config.ConnectToDB()
+	db := config.ConnectToDB()
+
+	if err := db.AutoMigrate(&model.User{}); err != nil {
+		log.Fatal("Error during migration: ", err)
+	}
 
 	port := "8080"
 	if envPort := config.GetEnv("PORT"); envPort != "" {
