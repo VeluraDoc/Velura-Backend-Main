a10c8721e2a4879eb0b080890b085e62ede858c0
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-30 16:45:56 +0330

    feat: implement password hashing and checking methods in User model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,31 @@
 package model
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID       string `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (u *User) HashPassword(password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return errors.New("failed to hash password")
+	}
+
+	u.Password = string(hash)
+
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+
+	return err == nil
 }
