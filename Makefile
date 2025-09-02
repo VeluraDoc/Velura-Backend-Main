442852f33ab4125430af837d2b27c4abaf13ae0f
AliDeWeb <AliMoradi0Business@gmail.com>
2025-09-02 17:35:24 +0330

    feat: separate api and cli logic

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
-SWAG_MAIN=cmd/main.go
+SWAG_MAIN=cmd/api/main.go
 SWAG_OUT=docs
 
 .PHONY: doc dev start build
 doc:
 	swag init -g ./$(SWAG_MAIN) -o ./$(SWAG_OUT) --parseDependency --parseInternal
 
-dev:
-	gin --appPort 5000 --port 3000 --build ./cmd --path . --bin ./bin/dev --all
+start-cli:
+	go run ./cmd/cli/main.go $(ARGS)
 
-start:
-	go run ./cmd/main.go
+start-api:
+	go run ./cmd/api/main.go
 
 build:
 	go build -o ./bin/production ./cmd
--- internal/module/pdf/usecase/pdf_convertor_usecase.go
+++ internal/module/pdf/usecase/pdf_convertor_usecase.go
@@ -3,18 +3,34 @@ package pdf_usecase
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
 )
 
 func PdfToDocx(input, output string) error {
+	if input == "" || output == "" {
+		return errors.New("empty input or output")
+	}
+
+	if _, err := os.Stat(input); err != nil {
+		return errors.New("input file does not exists")
+	}
+
+	filePath, err := filepath.Abs(input)
+
+	if err != nil {
+		return errors.New("input file does not exists")
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 
 	scriptPath, _ := filepath.Abs("tools/pdf_convertor.py")
-	cmd := exec.CommandContext(ctx, "python3", scriptPath, "-i", input, "-o", output)
+	cmd := exec.CommandContext(ctx, "python3", scriptPath, "-i", filePath, "-o", output)
 
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
